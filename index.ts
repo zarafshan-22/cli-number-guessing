d4b1237cc81256a520cce59b2fd68035ceffc08b
zarafshan-22 <zarafshan365@gmail.com>
2024-03-23 15:15:51 +0500

    Created Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber);
+const answer = await inquirer.prompt([
+    {
+        name:"UserGuessedNumber",
+        type:"number",
+        message:"Please  guess a number Between 1-6:",
+        }
+]);
+console.log(answer);
+
+if(answer.UserGuessedNumber ===randomNumber){
+console.log("Congratulation! You Guessed Right Number");
+} else{
+    console.log("You Guessed Wrong Number");
+};
